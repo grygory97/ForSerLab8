f9badc2a3d42376ace44a05847bd9ed662933fb6
Grzegorz <grzegorz.szawula@pollub.edu.pl>
2020-11-30 21:28:01 +0100

    Lab8 end

--- app/src/main/java/com/grzegorzszawula/forser/MainActivity.java
+++ app/src/main/java/com/grzegorzszawula/forser/MainActivity.java
@@ -3,24 +3,52 @@ package com.grzegorzszawula.forser;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button_start;
-    private Button button_stop;
-    private Button button_restart;
+    private Button button_start, button_stop, button_restart;
+    private TextView textInfoService, textInfoSettings;
+    private String message;
+    private Boolean show_time, work, work_double;
 
     private void startUI() {
         button_start = (Button) findViewById(R.id.buttonStart);
         button_stop = (Button) findViewById(R.id.buttonStop);
         button_restart = (Button) findViewById(R.id.buttonRestart);
+        textInfoService = (TextView) findViewById(R.id.textInfoServiceState);
+        textInfoSettings = (TextView) findViewById(R.id.textInfoSettings);
+    }
+
+    private void updateUI() {
+        if(isMyForegroundServiceRunning()){
+            button_start.setEnabled(false);
+            button_stop.setEnabled(true);
+            button_restart.setEnabled(true);
+            textInfoService.setText(getString(R.string.info_service_running));
+        }
+        else {
+            button_start.setEnabled(true);
+            button_stop.setEnabled(false);
+            button_restart.setEnabled(false);
+            textInfoService.setText(getString(R.string.info_service_not_running));
+        }
+
+        textInfoSettings.setText(getPreferences());
     }
 
     private void setupListeners() {
@@ -44,12 +72,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private String getPreferences() {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        message = sharedPreferences.getString("message", "ForSer");
+        show_time = sharedPreferences.getBoolean("show_time", true);
+        work = sharedPreferences.getBoolean("sync", true);
+        work_double = sharedPreferences.getBoolean("double", false);
+
+        return "Message: " + message + "\n"
+                + "show_time: " + show_time.toString() + "\n"
+                + "work: " + work.toString() + "\n"
+                + "double: " + work_double.toString();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startUI();
         setupListeners();
+        updateUI();
     }
 
     @Override
@@ -72,11 +115,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressWarnings("deprecation")
+    private boolean isMyForegroundServiceRunning(){
+
+        String myServiceName = MyForegroundService.class.getName();
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        for(ActivityManager.RunningServiceInfo runningService : activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            String runningServiceName = runningService.service.getClassName();
+            if(runningServiceName.equals(myServiceName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void clickStart(View view) {
+        getPreferences();
+
+        Intent startIntent = new Intent(this, MyForegroundService.class);
+        startIntent.putExtra(MyForegroundService.MESSAGE, message);
+        startIntent.putExtra(MyForegroundService.TIME, show_time);
+        startIntent.putExtra(MyForegroundService.WORK, work);
+        startIntent.putExtra(MyForegroundService.WORK_DOUBLE, work_double);
+        ContextCompat.startForegroundService(this, startIntent);
+        updateUI();
         Toast.makeText(this, "Start", Toast.LENGTH_SHORT).show();
     }
 
     public void clickStop(View view) {
+        Intent stopIntent = new Intent(this, MyForegroundService.class);
+        stopService(stopIntent);
+        updateUI();
         Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
     }
 
